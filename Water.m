ae7ba4c2538f354afa3bccf86408dec66e805ee5
Shaun Merritt <smerrittdevelopment@gmail.com>
2014-10-03 09:32:17 -0700

    Commit

---
+++ Water.m
@@ -0,0 +1,17 @@
+//
+//  Water.m
+//  Elu
+//
+//  Created by Shaun Merritt on 10/3/14.
+//  Copyright (c) 2014 True Merit Development. All rights reserved.
+//
+
+#import "Water.h"
+
+
+@implementation Water
+
+@dynamic numberOfCups;
+@dynamic dateLogged;
+
+@end
